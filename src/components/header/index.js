37defaaae20ab2fd7a0b2bbc638d52d3eeb59e60
batuhanerdogmus <72047013+batuhanerdogmus@users.noreply.github.com>
2021-01-23 18:49:50 +0300

    browse page created

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,13 +1,61 @@
 import React from "react";
 import { Link as ReactRouterLink } from "react-router-dom";
-import { Bacground, ButtonLink, Container, Logo } from "./styles/header";
+import {
+  Bacground,
+  ButtonLink,
+  Container,
+  Logo,
+  Link,
+  Group,
+  Picture,
+  Profile,
+  Dropdown,
+  Feature,
+  FeatureCallOut,
+  Text,
+} from "./styles/header";
 
 export default function Header({ bg = true, children, ...restProps }) {
   return bg ? <Bacground {...restProps}> {children} </Bacground> : children;
 }
 
+Header.Feature = function HeaderFeature({ children, ...restProps }) {
+  return <Feature {...restProps}> {children} </Feature>;
+};
+
+Header.Text = function HeaderText({ children, ...restProps }) {
+  return <Text {...restProps}> {children} </Text>;
+};
+
+Header.Profile = function HeaderProfile({ children, ...restProps }) {
+  return <Profile {...restProps}> {children} </Profile>;
+};
+
+Header.Picture = function HeaderPicture({ src, ...restProps }) {
+  return <Picture {...restProps} src={`/images/users/${src}.png`} />;
+};
+
+Header.Dropdown = function HeaderDropdown({ children, ...restProps }) {
+  return <Dropdown {...restProps}> {children} </Dropdown>;
+};
+
+Header.TextLink = function HeaderTextLink({ children, ...restProps }) {
+  return <Link {...restProps}> {children} </Link>;
+};
+
+Header.FeatureCallOut = function HeaderFeatureCallOut({
+  children,
+  ...restProps
+}) {
+  return <FeatureCallOut {...restProps}> {children} </FeatureCallOut>;
+};
+
 Header.Frame = function HeaderFrame({ children, ...restProps }) {
   return <Container {...restProps}> {children} </Container>;
+};
+
+Header.Group = function HeaderGroup({ children, ...restProps }) {
+  return <Group {...restProps}> {children} </Group>;
 };
 
 Header.ButtonLink = function HeaderButtonLink({ children, ...restProps }) {
--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,7 +1,10 @@
 import React, { useContext, useEffect, useState } from "react";
 import { SelectProfileContainer } from "./profiles";
 import { FirebaseContext } from "../context/firebase";
-import { Loading } from "../components";
+import { Header, Loading } from "../components";
+import logo from "../logo.svg";
+import * as ROUTES from "../constants/routes";
+
 export function BrowseContainer({ slides }) {
   const [profile, setProfile] = useState({});
   const [loading, setLoading] = useState(true);
@@ -16,11 +19,37 @@ export function BrowseContainer({ slides }) {
   }, [profile.displayName]);
 
   return profile.displayName ? (
-    loading ? (
-      <Loading src={user.photoURL} />
-    ) : (
-      <Loading.ReleseBody />
-    )
+    <>
+      {loading ? <Loading src={user.photoURL} /> : <Loading.ReleseBody />}
+      <Header src="joker1" dontShowOnSmallViewport>
+        <Header.Frame>
+          <Header.Group>
+            <Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix" />
+            <Header.TextLink>Series</Header.TextLink>
+            <Header.TextLink>Films</Header.TextLink>
+          </Header.Group>
+          <Header.Group>
+            <Header.Profile>
+              <Header.Picture src={user.photoURL} />
+              <Header.Dropdown>
+                <Header.Group>
+                  <Header.Picture src={user.photoURL} />
+                  <Header.TextLink>{user.displayName}</Header.TextLink>
+                </Header.Group>
+              </Header.Dropdown>
+            </Header.Profile>
+          </Header.Group>
+        </Header.Frame>
+        <Header.Feature>
+          <Header.FeatureCallOut>Watch Joker Now</Header.FeatureCallOut>
+          <Header.Text>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta quasi
+            id ex nostrum quo doloribus quos cupiditate velit, molestias nihil
+            quam accusamus tempora ut dolorem, odit natus corrupti minus aut.
+          </Header.Text>
+        </Header.Feature>
+      </Header>
+    </>
   ) : (
     <SelectProfileContainer user={user} setProfile={setProfile} />
   );
